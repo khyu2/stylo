7347a972886e788d8aa386231dac9c02edc46f76
khyu2 <ikeelee7742@gmail.com>
2025-08-19 10:29:38 +0900

    feat: tailwindcss 적용, 404, index 페이지 수정

---
+++ src/main/kotlin/project/stylo/common/response/BaseResponse.kt
@@ -0,0 +1,62 @@
+package project.stylo.common.response
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+@JsonPropertyOrder("code", "message", "data", "pagination")
+data class BaseResponse<T>(
+    val success: Boolean = true,
+    val message: String = "요청에 성공하였습니다.",
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    val data: T? = null,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    val pagination: PaginationInfo? = null
+) {
+    data class PaginationInfo(
+        val currentPage: Int,
+        val totalPages: Int,
+        val totalElements: Long,
+        val pageSize: Int,
+        val hasNext: Boolean,
+        val hasPrevious: Boolean
+    )
+
+    companion object {
+        @JvmStatic
+        fun <T> success(data: T): BaseResponse<T> {
+            return BaseResponse(true, "요청에 성공하였습니다.", data)
+        }
+
+        @JvmStatic
+        fun <T> success(message: String, data: T): BaseResponse<T> {
+            return BaseResponse(true, message, data)
+        }
+
+        @JvmStatic
+        fun <T> success(data: T, pagination: PaginationInfo): BaseResponse<T> {
+            return BaseResponse(true, "요청에 성공하였습니다.", data, pagination)
+        }
+
+        @JvmStatic
+        fun <T> success(message: String, data: T, pagination: PaginationInfo): BaseResponse<T> {
+            return BaseResponse(true, message, data, pagination)
+        }
+
+        @JvmStatic
+        fun <T> success(): BaseResponse<T> {
+            return BaseResponse(true, "요청에 성공하였습니다.")
+        }
+
+        @JvmStatic
+        fun <T> error(message: String): BaseResponse<T> {
+            return BaseResponse(false, message)
+        }
+
+        @JvmStatic
+        fun <T> error(message: String, data: T): BaseResponse<T> {
+            return BaseResponse(false, message, data)
+        }
+    }
+}
