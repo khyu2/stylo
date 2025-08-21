66fcd91d5a0d8965542ffe19f5e07ac7c76aeef5
khyu2 <ikeelee7742@gmail.com>
2025-08-21 12:06:33 +0900

    fix: 슬로우 쿼리 탐지 구현

--- src/main/kotlin/project/stylo/common/config/JooqConfig.kt
+++ src/main/kotlin/project/stylo/common/config/JooqConfig.kt
@@ -3,10 +3,12 @@ package project.stylo.common.config
 import org.jooq.ExecuteContext
 import org.jooq.ExecuteListener
 import org.jooq.conf.RenderKeywordCase
+import org.jooq.tools.StopWatch
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.jooq.DefaultConfigurationCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.time.Duration
 
 @Configuration
 class JooqConfig {
@@ -21,12 +23,27 @@ class JooqConfig {
         }
 }
 
-class SqlExecutor : ExecuteListener {
+class SqlExecutor() : ExecuteListener {
+    private lateinit var watch: StopWatch
+
     companion object {
         private val logger = LoggerFactory.getLogger(SqlExecutor::class.java)
+        private val SLOW_QUERY_THRESHOLD_MS = Duration.ofSeconds(2)
+    }
+
+    override fun executeStart(ctx: ExecuteContext?) {
+        watch = StopWatch()
     }
 
     override fun executeEnd(ctx: ExecuteContext) {
-        logger.info("SQL 실행: ${ctx.query()}")
+        val elapsedTime = watch.split()
+        val elapsedTimeS = String.format("%.2f", elapsedTime / 1_000_000_000.0) // ns -> seconds
+
+        if (elapsedTime > SLOW_QUERY_THRESHOLD_MS.toNanos()) {
+            logger.warn("실행된 쿼리 중 ${SLOW_QUERY_THRESHOLD_MS.seconds}초가 지난 쿼리가 있습니다." +
+                    " 실행시간: ${elapsedTimeS}s - SQL: ${ctx.query()}")
+        } else {
+            logger.info("SQL 실행 완료: ${elapsedTimeS}s - ${ctx.query()}")
+        }
     }
 }
