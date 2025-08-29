6c34273835b61a39b106b44409d67583903f5cba
khyu2 <ikeelee7742@gmail.com>
2025-08-30 00:47:31 +0900

    fix: unused converter 제거

--- src/main/kotlin/project/stylo/common/config/WebConfig.kt
+++ src/main/kotlin/project/stylo/common/config/WebConfig.kt
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.format.FormatterRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import project.stylo.common.converter.OptionMapListConverter
 import project.stylo.common.converter.OptionMapListGenericConverter
 import project.stylo.web.interceptor.CartInterceptor
 
