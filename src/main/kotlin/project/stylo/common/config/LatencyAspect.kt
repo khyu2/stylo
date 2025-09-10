31308573cc32f169fc6509c336701055c6e338ff
khyu2 <ikeelee7742@gmail.com>
2025-09-10 12:22:51 +0900

    feat: AOP 를 통한 api latency 측정 유틸 구현

---
+++ src/main/kotlin/project/stylo/common/config/LatencyAspect.kt
@@ -0,0 +1,28 @@
+package project.stylo.common.config
+
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Aspect
+@Component
+class LatencyAspect {
+    companion object {
+        private val logger = LoggerFactory.getLogger(LatencyAspect::class.java)
+    }
+
+    @Around(
+        "@annotation(org.springframework.web.bind.annotation.GetMapping) || " +
+                "@annotation(org.springframework.web.bind.annotation.PostMapping)"
+    )
+    fun measureLatency(joinPoint: ProceedingJoinPoint): Any? {
+        val start = System.nanoTime()
+        val result = joinPoint.proceed()
+        val end = System.nanoTime()
+        val durationMs = (end - start) / 1_000_000
+        logger.info("API 경로: ${joinPoint.target.javaClass.simpleName}.${joinPoint.signature.name} 응답시간: ${durationMs}ms")
+        return result
+    }
+}
