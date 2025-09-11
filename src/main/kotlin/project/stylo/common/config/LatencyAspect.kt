de9912326bdbe7ac27a6f0af8ad453515e38a4ac
khyu2 <ikeelee7742@gmail.com>
2025-09-11 17:00:14 +0900

    fix: logging api 경로 수정

--- src/main/kotlin/project/stylo/common/config/LatencyAspect.kt
+++ src/main/kotlin/project/stylo/common/config/LatencyAspect.kt
@@ -5,6 +5,8 @@ import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
 
 @Aspect
 @Component
@@ -34,8 +36,13 @@ class LatencyAspect {
         val end = System.nanoTime()
         val durationMs = (end - start) / 1_000_000
         val count = getAndResetQueryCount()
+
+        val request = (RequestContextHolder.currentRequestAttributes() as ServletRequestAttributes).request
+        val uri = request.requestURI
+        val method = request.method
+
         val logMessage =
-            "API 경로: ${joinPoint.target.javaClass.simpleName}.${joinPoint.signature.name} 응답시간: ${durationMs}ms, 쿼리 호출: ${count}회"
+            "API 경로: [${method}] $uri 응답시간: ${durationMs}ms, 쿼리 호출: ${count}회"
         if (durationMs >= 3000) {
             logger.warn("API 응답시간이 3초를 초과했습니다. $logMessage")
         } else {
